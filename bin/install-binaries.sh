21c482a374a4b758e2058816574692cf54a75821
Timothée Pillard <tim@gandi.net>
2019-02-03 23:57:12 +0100

    Update youtube-dl or install it

--- bin/install-binaries.sh
+++ bin/install-binaries.sh
@@ -1,9 +1,14 @@
 cd /home/hosting-user
-wget https://yt-dl.org/downloads/latest/youtube-dl
-chmod a+rx /home/hosting-user/youtube-dl
+
+if [ -f youtube-dl ]; then
+  youtube-dl -U
+else
+  wget https://yt-dl.org/downloads/latest/youtube-dl
+  chmod a+rx /home/hosting-user/youtube-dl
+fi
 
 wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
 tar xf ffmpeg-release-64bit-static.tar.xz
 rm ffmpeg-release-64bit-static.tar.xz
-mv ffmpeg-*-64bit-static/ffmpeg /home/hosting-user/ffmpeg
+mv ffmpeg-*-64bit-static/ffmpeg ffmpeg
 rm -rf ffmpeg-*-64bit-static/
