0a72c0a2e22d47ffaba9ec392e07d5555e5ba038
Timothée Pillard <tim@gandi.net>
2019-02-03 23:41:40 +0100

    mhh maybe not

--- bin/install-binaries.sh
+++ bin/install-binaries.sh
@@ -5,5 +5,5 @@ chmod a+rx /home/hosting-user/youtube-dl
 wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
 tar xf ffmpeg-release-64bit-static.tar.xz
 rm ffmpeg-release-64bit-static.tar.xz
-mv ffmpeg-4.0.2-64bit-static/ffmpeg /home/hosting-user/ffmpeg
-rm -rf ffmpeg-4.0.2-64bit-static/
+mv ffmpeg-*-64bit-static/ffmpeg /home/hosting-user/ffmpeg
+rm -rf ffmpeg-*-64bit-static/
