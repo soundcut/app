d499864ed3506938a56a09c4b8339119d042d01c
Tim <tim@tpillard.fr>
2019-03-19 13:08:41 +0100

    No whitespace in <canvas>
    May help devtools crashing in Safari

--- shared/components/WaveForm.js
+++ shared/components/WaveForm.js
@@ -21,36 +21,11 @@ const PROGRESS_COLOR = '#24adc2';
 
 function Canvases(containerWidth, width) {
   return wire()`
-    <canvas
-      id="waveform-canvas"
-      width="${width}"
-      height="${HEIGHT}"
-    >
-    </canvas>
-    <canvas
-      id="progress-canvas"
-      width="${width}"
-      height="${HEIGHT}"
-    >
-    </canvas>
-    <canvas
-      id="duration-canvas"
-      width="${containerWidth}"
-      height="${HEIGHT}"
-    >
-    </canvas>
-    <canvas
-      id="start-canvas"
-      width="${containerWidth}"
-      height="${HEIGHT}"
-    >
-    </canvas>
-    <canvas
-      id="end-canvas"
-      width="${containerWidth}"
-      height="${HEIGHT}"
-    >
-    </canvas>
+    <canvas id="waveform-canvas" width="${width}" height="${HEIGHT}"></canvas>
+    <canvas id="progress-canvas" width="${width}" height="${HEIGHT}"></canvas>
+    <canvas id="duration-canvas" width="${containerWidth}" height="${HEIGHT}"></canvas>
+    <canvas id="start-canvas" width="${containerWidth}" height="${HEIGHT}"></canvas>
+    <canvas id="end-canvas" width="${containerWidth}" height="${HEIGHT}"></canvas>
   `;
 }
 
