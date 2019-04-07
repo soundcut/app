eb531e63e9dca520b8334993c2a85f7fed3dd5b2
Tim <tim@tpillard.fr>
2019-04-07 12:12:18 +0200

    Attempt to get PWA install prompt

--- client/index.js
+++ client/index.js
@@ -42,3 +42,16 @@ if (document.readyState !== 'loading') {
 if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   navigator.serviceWorker.register('/service-worker.js');
 }
+
+window.addEventListener('beforeinstallprompt', function onBeforeInstallPrompt(
+  evt
+) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  evt.preventDefault();
+  const installBtn = document.getElementById('btn-install');
+  installBtn.addEventListener('click', function onInstallBtnClick() {
+    installBtn.parentNode.removeChild(installBtn);
+    evt.prompt();
+  });
+  installBtn.style.display = 'block';
+});
